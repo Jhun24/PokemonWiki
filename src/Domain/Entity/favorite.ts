ddff2844218f3f02ca7bee1f9c5519b62597b9f6
Jhun24 <jjanghuni102@naver.com>
2023-08-06 17:13:06 +0900

    refactor: Domain/Entity favorite

--- src/Domain/Entity/favorite.ts
+++ src/Domain/Entity/favorite.ts
@@ -28,8 +28,43 @@ type PokemonDataType = {
   url: string;
 };
 
-class FavoriteEntity {
+type ItemType = {
+  type: string;
   item: ItemDataType | PokemonDataType;
+};
+
+class FavoriteEntity {
+  item: ItemType[] | null;
+  type: string;
+  constructor() {
+    this.item = null;
+    this.type = 'all';
+  }
+
+  getItemList() {
+    if (this.item === null) return null;
+    if (this.type === 'pokemon') {
+      let returnItem: ItemType[] = [];
+      for (const d of this.item) {
+        if (d.type === 'pokemon') {
+          returnItem.push(d);
+        }
+      }
+      if(returnItem.length === 0) return null;
+      return returnItem;
+    }
+    if (this.type === 'item') {
+      let returnItem: ItemType[] = [];
+      for (const d of this.item) {
+        if (d.type === 'item') {
+          returnItem.push(d);
+        }
+      }
+      if(returnItem.length === 0) return null;
+      return returnItem;
+    }
+    return this.item;
+  }
 }
 
 export default FavoriteEntity;
